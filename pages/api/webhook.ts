548f2f26d515051307f708d17e7d1b8c34b4761d
jas <80989797+jaskirat05@users.noreply.github.com>
2024-12-16 12:17:19 +0530

    build error unused var

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import cloudinary from 'cloudinary';
+//import cloudinary from 'cloudinary';
 import { supabaseAdmin } from '../../lib/supabase';
 
 export const config = {
